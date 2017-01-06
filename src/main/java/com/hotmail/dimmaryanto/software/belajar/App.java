3c0d26b12dff7261c2c237e33e629b336477f60c
dimmaryanto93@aspire-e5 <software.dimmaryanto@hotmail.com>
2017-01-06 21:29:43 +0700

    optimaze code

--- src/main/java/com/hotmail/dimmaryanto/software/belajar/App.java
+++ src/main/java/com/hotmail/dimmaryanto/software/belajar/App.java
@@ -1,8 +1,15 @@
 package com.hotmail.dimmaryanto.software.belajar;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        HibernateFactory factory = new HibernateFactory();
+        SessionFactory sessionFactory = factory.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.close();
+        sessionFactory.close();
+    }
 }
--- src/main/java/com/hotmail/dimmaryanto/software/belajar/HibernateFactory.java
+++ src/main/java/com/hotmail/dimmaryanto/software/belajar/HibernateFactory.java
@@ -1,40 +1,32 @@
 package com.hotmail.dimmaryanto.software.belajar;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateFactory {
-	
-	private SessionFactory sessionFactory;
 
-	public SessionFactory getSessionFactory() {
-		// A SessionFactory is set up once for an application!
-		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				.configure() // configures settings from hibernate.cfg.xml
-				.build();
-		try {
-			sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-		}catch (Exception e) {
-			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-			// so destroy it manually.
-			StandardServiceRegistryBuilder.destroy( registry );
-		}
-		return sessionFactory;
-	}
+    private SessionFactory sessionFactory;
 
-	private void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
-	
-	public static void main(String[] args){
-		HibernateFactory factory = new HibernateFactory();
-		SessionFactory sessionFactory = factory.getSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.close();
-		sessionFactory.close();
-	}
+    public SessionFactory getSessionFactory() {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+        return sessionFactory;
+    }
+
+    private void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
 
 }
